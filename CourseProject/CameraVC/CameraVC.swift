a87f57e746d8752df92bbd777d5e93c656faf8be
AllanLysiuk <allan.lysiuk@gmail.com>
2023-04-25 00:00:02 +0300

    1.Added MlKit podFile 2.Added cameraVC

--- CourseProject/CameraVC/CameraVC.swift
+++ CourseProject/CameraVC/CameraVC.swift
@@ -7,9 +7,46 @@
 
 import UIKit
 
-final class CameraVC: UIViewController{
+final class CameraVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    private let vc = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+        vc.delegate = self
+        vc.allowsEditing = true
+        imageView.image = UIImage(named: "emptyBottleOfWine")
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        imageView.image = image
+    }
+    
+    @IBAction func getInfoDidTap() {
+        
+        if imageView.image == UIImage(named: "emptyBottleOfWine") {
+            print("bad")
+        } else {
+            print("Good")
+        }
+    }
+    
+    @IBAction func openCameraDidTap() {
+        vc.sourceType = .camera
+        present(vc, animated: true)
+    }
+    
+    @IBAction func openLibraryDidTap() {
+        vc.sourceType = .photoLibrary
+        present(vc, animated: true)
     }
 }
